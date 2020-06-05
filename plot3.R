81f903ae120c11264ba0defd6fd962b3e3601f8b
Desan1985 <kontakt@martinschultze.de>
2020-06-05 08:54:28 +0200

    problem solved with legend

--- plot3.R
+++ plot3.R
@@ -31,10 +31,9 @@ df_full$datetime2 <- ymd_hms(df_full$datetime)
 df_2days <- df_full[(df_full$Date3 == "2007-02-01" | df_full$Date3 == "2007-02-02"),]
 
 ###plot3
+png(file= "plot3.png",width=480, height=480, units = "px")
 with(df_2days, plot(datetime2, Sub_metering_1, type="l",xlab="", ylab= "Energy sub metering"))
 lines(df_2days$datetime2, df_2days$Sub_metering_2, col="red")
 lines(df_2days$datetime2, df_2days$Sub_metering_3, col="blue")
 legend("topright", lwd = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-dev.copy(png, file= "plot3.png",width=480, height=480, units = "px")
 dev.off()
